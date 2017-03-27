fa1d054c3a804386cd338858ea5bd8599a0da0c3
Horace Kyuho Lee <KyuhoLee@Leeui-MacBook-Pro.local>
2017-03-28 01:01:38 +0900

    add android

---
+++ Android/CoffeePlease/app/src/main/java/com/nice295/coffeeplease/IntroActivity.java
@@ -0,0 +1,30 @@
+package com.nice295.coffeeplease;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class IntroActivity extends AppCompatActivity {
+
+    private Handler mHandler;
+    private Runnable mRunnable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        mRunnable = new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+
+        mHandler = new Handler();
+        mHandler.postDelayed(mRunnable, 3000);
+    }
+}
---
+++ Android/CoffeePlease/app/src/main/java/com/nice295/coffeeplease/MainActivity.java
@@ -0,0 +1,13 @@
+package com.nice295.coffeeplease;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
